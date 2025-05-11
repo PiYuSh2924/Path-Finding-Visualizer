c1faddc266a92aea7ccf94d958d33102498993b8
PiYuSh2924 <piyushsharma2924@gmail.com>
2025-05-11 13:51:49 +0530

    Created Contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { PathFindingProvider } from "./context/PathFindingContext";
+import { TileProvider } from "./context/TileContext";
+import { SpeedProvider } from "./context/SpeedContext";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-      Hello World!
-    </h1>
+    <PathFindingProvider>
+      <TileProvider>
+        <SpeedProvider>  
+          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+            Hello World!
+          </h1>
+        </SpeedProvider>
+      </TileProvider>
+    </PathFindingProvider>
   );
 };
 
